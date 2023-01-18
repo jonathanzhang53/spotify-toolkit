53e05291fa3dab922fb014d121e7501fa86d6acf
jonathanzhang53 <jonathanzhang53@gmail.com>
2023-01-18 15:54:58 -0500

    separated API query and analytics

--- p1.py
+++ p1.py
@@ -53,8 +53,8 @@ def collate_songs_from_playlists(playlists):
 
         for song in playlist_songs:
             if not song['track']: # TODO FOR PODCAST TRACKS
-                song_name = "PODCAST"
-                song_id = ""
+                song_name = 'PODCAST'
+                song_id = ''
                 print(song)
             else:
                 # print(song['track']['name'])
@@ -63,9 +63,9 @@ def collate_songs_from_playlists(playlists):
                 song_id = song['track']['id']
 
                 if not song['track']['id']:
-                    song_id = "LOCAL"
+                    song_id = 'LOCAL'
 
-            songs[song_name + " | " + song_id].append(playlist['name'])
+            songs[song_name + ' | ' + song_id].append(playlist['name'])
     
     return songs
 
@@ -75,36 +75,8 @@ my_playlists = load_user_playlists(user_id)
 songs = collate_songs_from_playlists(my_playlists)
 
 # WRITE SONGS DEFAULTDICT TO JSON
-with open('myfile.json', 'w', encoding ='utf8') as json_file:
+with open('songs.json', 'w', encoding = 'utf8') as json_file:
     json.dump(songs, json_file, ensure_ascii = True)
-
-# EXTRACT ANALYTICS FROM SONGS DATA TODO
-num_playlists = str(len(my_playlists))
-
-non_track_entries = str(len(songs["PODCAST"]))
-
-total_songs = str(len(songs)) # may be off by 1 due to podcast entry
-
-duplicate_songs = defaultdict(list)
-
-for key in songs.keys():
-    if len(songs[key]) > 1 and key != "PODCAST":
-        duplicate_songs[key].append(songs[key])
-
-# WRITE ANALYTICS TO OUTPUT TEXT FILE
-with open('results.txt', 'w', encoding ='utf8') as f:
-    f.write('Number of playlists: ' + num_playlists + '\n')
-    f.write('Number of non-track entries: ' + non_track_entries + '\n')
-    f.write('Total songs: ' + total_songs + '\n')
+with open('playlists.json', 'w', encoding = 'utf8') as json_file:
+    json.dump(my_playlists, json_file, ensure_ascii = True)
     
-    f.write('\n' + 'Songs Duplicated Across Playlists:' + '\n')
-
-    for dup in duplicate_songs.items():
-        song = dup[0]
-        dup_playlists = dup[1][0]
-        print(song)
-        print(dup_playlists)
-
-        f.write(song + '\n')
-        for playlist in dup_playlists:
-            f.write('\t' + playlist + '\n')
--- p2.py
+++ p2.py
@@ -2,35 +2,42 @@ import json
 from collections import defaultdict
 
 ### LOAD FROM JSON
+with open('songs.json', 'r') as f:
+    songs = json.load(f)
+with open('playlists.json', 'r') as f:
+    playlists = json.load(f)
+
 ### CONVERT TO songs DEFAULTDICT
+print(songs)
+# print(playlists)
 
 # EXTRACT ANALYTICS FROM SONGS DATA TODO
-# num_playlists = str(len(my_playlists))
+num_playlists = str(len(playlists))
 
-# non_track_entries = str(len(songs["PODCAST"]))
+non_track_entries = str(len(songs['PODCAST | ']))
 
-# total_songs = str(len(songs)) # may be off by 1 due to podcast entry
+total_songs = str(len(songs)) # may be off by 1 due to podcast entry
 
-# duplicate_songs = defaultdict(list)
+duplicate_songs = defaultdict(list)
 
-# for key in songs.keys():
-#     if len(songs[key]) > 1 and key != "PODCAST":
-#         duplicate_songs[key].append(songs[key])
+for key in songs.keys():
+    if len(songs[key]) > 1 and key != 'PODCAST | ':
+        duplicate_songs[key].append(songs[key])
 
 # WRITE ANALYTICS TO OUTPUT TEXT FILE
-# with open('results.txt', 'w', encoding ='utf8') as f:
-#     f.write('Number of playlists: ' + num_playlists + '\n')
-#     f.write('Number of non-track entries: ' + non_track_entries + '\n')
-#     f.write('Total songs: ' + total_songs + '\n')
+with open('results.txt', 'w', encoding ='utf8') as f:
+    f.write('Number of playlists: ' + num_playlists + '\n')
+    f.write('Number of non-track entries: ' + non_track_entries + '\n')
+    f.write('Total songs: ' + total_songs + '\n')
     
-#     f.write('\n' + 'Songs Duplicated Across Playlists:' + '\n')
+    f.write('\n' + 'Songs Duplicated Across Playlists:' + '\n')
 
-#     for dup in duplicate_songs.items():
-#         song = dup[0]
-#         dup_playlists = dup[1][0]
-#         print(song)
-#         print(dup_playlists)
+    for dup in duplicate_songs.items():
+        song = dup[0]
+        dup_playlists = dup[1][0]
+        print(song)
+        print(dup_playlists)
 
-#         f.write(song + '\n')
-#         for playlist in dup_playlists:
-#             f.write('\t' + playlist + '\n')
+        f.write(song + '\n')
+        for playlist in dup_playlists:
+            f.write('\t' + playlist + '\n')
